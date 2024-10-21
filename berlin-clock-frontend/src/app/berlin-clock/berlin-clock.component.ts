bdd6f55913a465f896763df86e98c75eee8f9d85
mdurmus <mehmet.durmus@soprasteria.com>
2024-10-21 23:23:11 +0200

    feat: added digital font for time and bootstrap 5 to the front end

--- berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
+++ berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
@@ -12,11 +12,20 @@ import {WebsocketService} from '../websocket/websocket.service';
 export class BerlinClockComponent {
 
   timeData: any;
+  currentTime: any;
 
   constructor(private websocketService: WebsocketService) {
     this.websocketService.getBerlinClockUpdates().subscribe((data) => {
       console.log(data)
       this.timeData = data
+      this.currentTime = this.getCurrentDigitalTime()
     });
+  }
+
+  getCurrentDigitalTime(): string {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
   }
 }
