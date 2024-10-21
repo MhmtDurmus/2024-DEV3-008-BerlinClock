e2c502212a36ad2446d0e30b3a0a108c60bf9f90
mdurmus <mehmet.durmus@soprasteria.com>
2024-10-21 14:40:06 +0200

    created front-end-project

---
+++ berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-berlin-clock',
+  standalone: true,
+  imports: [],
+  templateUrl: './berlin-clock.component.html',
+  styleUrl: './berlin-clock.component.scss'
+})
+export class BerlinClockComponent {
+
+}
