5264746965c375b5500fb617c11f85f7ec85877e
mdurmus <mehmet.durmus@soprasteria.com>
2024-10-21 14:51:28 +0200

    added berlin-clock comp and router-outlet

--- berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
+++ berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-berlin-clock',
@@ -9,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class BerlinClockComponent {
 
-}
+  timeData: any;  // Variable to hold time data
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getBerlinClockTime();
+  }
+
+  getBerlinClockTime(): void {
+    this.http.get('http://localhost:8080/api/berlinclock')  // Call your backend API
+      .subscribe((data) => {
+        this.timeData = data;
+      });
+  }}
