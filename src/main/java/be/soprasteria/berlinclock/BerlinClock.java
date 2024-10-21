396cd444c63d3c1bd654fa74e7aa12284d9c31dc
mdurmus <mehmet.durmus@soprasteria.com>
2024-10-21 16:24:09 +0200

    five hours step

--- src/main/java/be/soprasteria/berlinclock/BerlinClock.java
+++ src/main/java/be/soprasteria/berlinclock/BerlinClock.java
@@ -5,4 +5,17 @@ public class BerlinClock {
     public String getSecondsLamp(int seconds) {
         return (seconds % 2 == 0) ? "Y" : "O";
     }
+
+    public String getFiveHourRow(int hours) {
+        int lampsOn = hours / 5;
+        return getLamps(lampsOn, 4, "R");
+    }
+
+    private String getLamps(int lampsOn, int totalLamps, String onSymbol) {
+        StringBuilder row = new StringBuilder();
+        for (int i = 0; i < totalLamps; i++) {
+            row.append(i < lampsOn ? onSymbol : "O");
+        }
+        return row.toString();
+    }
 }
--- src/test/java/be/soprasteria/berlinclock/BerlinClockApplicationTests.java
+++ src/test/java/be/soprasteria/berlinclock/BerlinClockApplicationTests.java
@@ -8,13 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class BerlinClockApplicationTests {
 
-
     @Test
-    void contextLoads() {
-    }
-
-    @Test
-    public void testEvenSecondsShouldTurnOnLamp() {
+    void testEvenSecondsShouldTurnOnLamp() {
         BerlinClock clock = new BerlinClock();
         assertEquals("Y", clock.getSecondsLamp(0));
         assertEquals("Y", clock.getSecondsLamp(2));
@@ -22,11 +17,20 @@ class BerlinClockApplicationTests {
     }
 
     @Test
-    public void testOddSecondsShouldTurnOffLamp() {
+    void testOddSecondsShouldTurnOffLamp() {
         BerlinClock clock = new BerlinClock();
         assertEquals("O", clock.getSecondsLamp(1));
         assertEquals("O", clock.getSecondsLamp(3));
         assertEquals("O", clock.getSecondsLamp(59));
+    }
+
+    @Test
+    void testFiveHourBlocks() {
+        BerlinClock clock = new BerlinClock();
+        assertEquals("OOOO", clock.getFiveHourRow(0));  // 0 hours, no lights
+        assertEquals("ROOO", clock.getFiveHourRow(5));  // 5 hours, 1 light
+        assertEquals("RROO", clock.getFiveHourRow(10)); // 10 hours, 2 lights
+        assertEquals("RRRR", clock.getFiveHourRow(20)); // 20 hours, 4 lights
     }
 
 }
