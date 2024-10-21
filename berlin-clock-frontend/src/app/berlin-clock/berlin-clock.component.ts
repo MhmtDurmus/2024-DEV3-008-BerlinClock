606b5be935dabe8b2501ca1cee3e5718c2f42565
mdurmus <mehmet.durmus@soprasteria.com>
2024-10-21 23:32:35 +0200

    feat: added method to get lamp in the template and show as visual.

--- berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
+++ berlin-clock-frontend/src/app/berlin-clock/berlin-clock.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {WebsocketService} from '../websocket/websocket.service';
 
 
 @Component({
   selector: 'app-berlin-clock',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './berlin-clock.component.html',
   styleUrl: './berlin-clock.component.scss'
 })
@@ -27,5 +28,14 @@ export class BerlinClockComponent {
     const hours = now.getHours().toString().padStart(2, '0');
     const minutes = now.getMinutes().toString().padStart(2, '0');
     return `${hours}:${minutes}`;
+  }
+
+  getLamps(lamps: string, colorCodes: string, total: number) {
+    const lampClasses = [];
+    for (let i = 0; i < total; i++) {
+      lampClasses.push(i < lamps.length && lamps[i] === colorCodes[0] ? 'yellow' :
+        (i < lamps.length && lamps[i] === colorCodes[1]) ? 'red' : 'off');
+    }
+    return lampClasses;
   }
 }
